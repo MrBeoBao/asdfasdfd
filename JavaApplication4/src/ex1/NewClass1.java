697c649efab4dd71744e6685a0bb11808b79278a
MrBeoBao <kinnamphuoc2@gmail.com>
2026-03-03 20:14:14 +0700

    Add files via upload

---
+++ JavaApplication4/src/ex1/NewClass1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kinna
+ */
+public class NewClass1 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("nhap ban kinh ");
+        double a = s.nextDouble();
+        System.out.println(a);
+        System.out.print("Nhap dien tich: ");
+        double b = s.nextDouble();
+    }
+}
